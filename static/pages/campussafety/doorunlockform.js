8991de81ff865e2aea24e8bab78ce5acf45930b0
Ashley Kang <kangashley@users.noreply.github.com>
2019-12-09 11:32:14 -0800

    Update Door Unlock Form

--- static/pages/campussafety/doorunlockform.js
+++ static/pages/campussafety/doorunlockform.js
@@ -20,8 +20,110 @@ class DoorUnlockPage extends HTMLElement {
           </ion-card-header>
 
           <h2>Request for Door Unlock</h2>
-           Insert form crap here
+           The requesting party must be present when the officer arrives. Please have your room permit and valid form of ID ready. If the name on your ID does not match the name on the room permit, access may not be granted. If you are requesting for an exterior door to be unlocked, please specify this and describe its location under "additional comments".
         </ion-card>
+
+<form action ="/submit" method = "POST">
+            <div class = "form-group">
+            <ion-list lines="full" class="ion-no-margin ion-no-padding">
+              <ion-item>
+                <ion-label position="stacked">
+                  First Name <ion-text color="danger">*</ion-text>
+                </ion-label>
+                <ion-input
+                  required
+                  type="text"
+                  name = "fName"
+                ></ion-input>
+              </ion-item>
+
+              <ion-item>
+                <ion-label position="stacked">
+                  Last Name <ion-text color="danger">*</ion-text>
+                </ion-label>
+                <ion-input
+                  required
+                  type="text"
+                  name = "lName"
+                ></ion-input>
+              </ion-item>
+
+              <ion-item>
+                <ion-label position="stacked">
+                  CWID <ion-text color="danger">*</ion-text>
+                </ion-label>
+                <ion-input
+                  required
+                  type="number"
+                  name = "iD"
+                ></ion-input>
+              </ion-item>
+
+              <ion-item>
+                <ion-label position="stacked">
+                  Phone Number <ion-text color="danger">*</ion-text>
+                </ion-label>
+                <ion-input
+                  required
+                  type="tel"
+                  name = "pNum"
+                ></ion-input>
+              </ion-item>
+
+              <ion-item>
+                <ion-label position="stacked">
+                  Email <ion-text color="danger">*</ion-text>
+                </ion-label>
+                <ion-input
+                  required
+                  type="email"
+                  name = "eMail"
+                ></ion-input>
+              </ion-item>
+
+              <ion-item>
+                <ion-label position="stacked">
+                  Building Name<ion-text color="danger">*</ion-text>
+                </ion-label>
+                <ion-input
+                  required
+                  type="text"
+                  name = "location"
+                </ion-item>
+
+              <ion-item>
+                <ion-label position="stacked">
+                  Room Number(s) <ion-text color="danger">*</ion-text>
+                </ion-label>
+                <ion-input
+                  required
+                  type="number"
+                  name = "roomNum"
+                ></ion-input>
+              </ion-item>
+
+            <ion-item>
+                <ion-label position="stacked">
+                  Additional Comments <ion-text color="medium">(optional)</ion-text>
+                </ion-label>
+                <ion-input
+                  type="text"
+                  name = "addComments"
+                ></ion-input>
+              </ion-item>
+
+
+          <ion-nav-push component="page-campussafety-confirmation">
+            <ion-button 
+              color="primary" 
+              expand="block"
+              type = "submit"
+              >Submit</ion-button>
+          </ion-nav-push>
+
+</br>
+<b><center><em>In case of emergency, call <a href="tel:911">911</a></em></center></b>
+</br>
 
         <ion-footer>
           <ion-toolbar>     
